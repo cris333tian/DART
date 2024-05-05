a4678049c2218e25f1e0e5a72d63135576acd585
cristian3228624186 <159269816+cristian3228624186@users.noreply.github.com>
2024-05-05 18:50:32 -0500

    Add files via upload

---
+++ EjemplosDart/EjemplosDart/tiposvariables02.dart
@@ -0,0 +1,31 @@
+void main() {
+print("TIPOS DE VARIABLES");
+print("Tipo ENTERO");
+int cantExamenes; //Definicion variable
+cantExamenes = 3; //Asiganción variable
+int cabtEstudiantes = 30; //Definicion/Asignación variable
+print("La cantidad de estudiantes es: $cabtEstudiantes");
+print("Debe presentar $cantExamenes examenes");
+print("*************************************");
+
+print("Tipo REAL");
+double peso; //Definicion
+peso = 70.5; //Asignación
+double promedio = 3.5; //Definicion y Asignacion
+print("Su peso es: $peso y su promedio es: $promedio");
+print("*******************************************");
+
+print("Tipo CADENA");
+String telefono;
+telefono = "3228624186";
+String email = "ana@gmail.com";
+print("Su telefono es: $telefono y su email es: $email");
+print("*********************************************");
+
+print("Tipo BOOLEANO");
+bool esCasado;
+esCasado = false;
+bool tieneHijos = true;
+print("Es casado: $esCasado");
+print("Tiene hijos: $tieneHijos");
+}
---
+++ EjemplosDart/EjerciciosGuia3/CondicionalesSimples/condicionalSimple02.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+void main() {
+  //Cristian Álvarez - Ejercicio Condicional 02
+  /*
+  La compañía de seguros de vida atlas se va a cambiar de domicilio y por lo tanto pone en venta su
+  terreno pero no tiene una idea del valor del terreno, entonces solicita al departamento de sistemas
+  que le desarrolle un algoritmo con la finalidad de que calcule e imprima el precio del terreno del cual
+  se tiene los siguientes datos: largo, ancho y precio por metro cuadrado, si el terreno tiene más de
+  400 metros cuadrados se hace un descuento del 10%
+  */
+  // DEFINICION DE VARIABLES
+  double largo, ancho, precioM2, precioTerreno, descuento;
+  double areaTerreno;
+  // ENTRADA ALGORITMO
+  print("Ingrese el largo del terreno:");
+  largo = double.parse(stdin.readLineSync()!);
+  print("Ingrese el ancho del terreno:");
+  ancho = double.parse(stdin.readLineSync()!);
+  print("Ingrese el precio del metro cuadrado:");
+  precioM2 = double.parse(stdin.readLineSync()!);
+  // PROCESO ALGORITMO
+  areaTerreno = largo * ancho;
+  precioTerreno = areaTerreno * precioM2;
+  if (areaTerreno > 400) {
+    descuento = precioTerreno *0.1;
+    precioTerreno = precioTerreno - descuento;
+    print("El descuento es de: $descuento");
+  }
+  // SALIDA ALGORITMO
+  print("El precio del terreno es: $precioTerreno");
+}
