ee7346e1a8cab509ba84e6b8ec83e5fa114e514f
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2024-05-07 11:13:07 -0500

    7/05/2024

--- ADSO2873711/Ciclos/EjemplosCiclos/ejemploFOR_02.dart
+++ ADSO2873711/Ciclos/EjemplosCiclos/ejemploFOR_02.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  print("Imprimir los números del 0 al 25 de 2 en 2");
+  print("Imprimir los números del 0 al 25, de 2 en 2");
   for (int i = 0; i <= 25; i+=2) {
     print("i: $i");
   }
