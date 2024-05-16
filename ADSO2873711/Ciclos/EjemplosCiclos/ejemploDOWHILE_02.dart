ab272cb96e599b66cd78f0bac41b6aadd3e16186
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2024-05-16 11:54:41 -0500

    16/05/2024

--- ADSO2873711/Ciclos/EjemplosCiclos/ejemploDOWHILE_02.dart
+++ ADSO2873711/Ciclos/EjemplosCiclos/ejemploDOWHILE_02.dart
@@ -5,34 +5,42 @@ void main(List<String> args) {
   Desarrollar un menu que permita realizar las operaciones matemáticas básicas
   */
   int opcion;
-  double resultado, num1, num2;
+  double resultado, num1 = 0, num2 = 0;
   do{
+    print("*****************************************");
     print("BIENVENIDO A LA CALCULADORA");
-    print("Digite la opción deseada:");
     print("1. Suma");
     print("2. Resta");
     print("3. Multiplicación");
     print("4. División");
     print("5. Salir");
+    print("*****************************************");
+    print("Digite la opción deseada:");
     opcion = int.parse(stdin.readLineSync()!);
-    switch (opcion) {
-    case 1:
-      print("Ingrese los números para Sumar:");
+      if (opcion >= 1 && opcion < 5) {
+      print("Ingrese los 2 números para operar:");
       num1 = double.parse(stdin.readLineSync()!);
       num2 = double.parse(stdin.readLineSync()!);
-      resultado = num1 + num2
+    }
+    switch (opcion) {
+    case 1:
+      resultado = num1 + num2;
+      print("La suma de los números es: $resultado");
       break;
     case 2:
-      print("Ingrese los números para Restar:");
+      resultado = num1 - num2;
+      print("La resta de los números es: $resultado");
       break;
     case 3:
-      print("Ingrese los números para Multiplicar:");
+      resultado = num1 * num2;
+      print("La multiplicación de los números es: $resultado");
       break;
     case 4:
-      print("Ingrese los números para Dividir:");
+      resultado = num1 / num2;
+      print("La división de los números es: $resultado");
       break;
     case 5:
-      print("En la buena!");
+      print("Haz salido de la CALCULADORA");
       break;
     default:
     print("Opción INCORRECTA");
